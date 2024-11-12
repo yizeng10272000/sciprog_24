4ba625bc4b3eaf59ed3eec891d4b65b9becc2041
yizeng10272000 <144727865+yizeng10272000@users.noreply.github.com>
2024-11-12 15:43:28 +0000

    update for practical8

--- practical8/exercise_8.c
+++ practical8/exercise_8.c
@@ -1,27 +1,42 @@
+
+/* Iterative and recursive versions of Eulid's algorithm to calculate 
+ gcd of two positive integers */
+
 #include<stdio.h>
 
-int gcd(int a, int b);
-
+int iterative(int a, int b);
+int recursive(int a, int b);
+ 
 int main(){
 
 // declare the variables
-int a, b;
+int a, b, error;
 
 // enter the number of a, b
-printf("please enter your number of a\n");
-scanf("%d", &a);
+printf("please enter your two positive integers of a, b\n");
+error = scanf("%d %d", &a, &b);
 
-printf("please enter your number of b\n");
-scanf("%d", &b);
+if (error != 2){
+  printf("please enter two positive integers.\n");
+  return 1;
+}
+if (a <= 0 || b <= 0){
+  printf("please enter positive integers.\n");
+  return 1;
+}
 
-printf("the greatest common divisor between %d and %d is %d\n", a, b, gcd(a, b));
+
+printf("the greatest common divisor calculated by iterative between %d and %d is %d\n", a, b, iterative(a, b));
+
+printf("the greatest common divisor calculated by recursive between %d and %d is %d\n", a, b, recursive(a, b));
+
 
 return 0;
 
 }
 
-// calculate the gcd
-int gcd(int a, int b){
+// calculate the gcd by iteration
+int iterative(int a, int b){
 
 while(b != 0){
   int temp;
@@ -33,5 +48,17 @@ while(b != 0){
 }
 
   return a;
+
+}
+
+// anoother way to calculate the gcd
+
+int recursive(int a, int b){
+  if(b == 0){
+    return a;
+  }
+  else{
+    return(recursive(b, a % b));
+  }
 
 }
